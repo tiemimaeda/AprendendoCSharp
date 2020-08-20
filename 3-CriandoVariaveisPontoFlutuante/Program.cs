4a63fe166415219483e7f927e1bffb58dd7be5b1
MAEDA Priscila (Consulting For ACCOR Hotels) <Priscila.MAEDA@consulting-for.accor.com>
2020-08-19 23:09:08 -0300

    aulas 1, 2 e 3

---
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 3 - Criando variáveis ponto flutuante");
+
+            double salario;
+            salario = 1200.70;
+            Console.WriteLine(salario);
+
+            //double admite números inteiros e decimais, depende de como vc usa
+            //Se um dos números tiver decimal, o resultado será com decimal, se nenhum
+            //dos números tiver decimal, o número será arredondado para inteiro
+            double idade;
+            idade = 15.0 / 2;
+            Console.WriteLine(idade);
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 4-ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 4");
+
+            double salario;
+            salario = 1200.50;
+
+            //Casting - conversão
+            //neste caso, o número perde a precisão
+            //int suporta 32bits
+            int salarioEmInteiro;
+            salarioEmInteiro = (int)salario;
+            Console.WriteLine(salarioEmInteiro);
+
+            //long suporta 64bits
+            long idade = 130000000000;
+            Console.WriteLine(idade);
+
+            //short suporta 16bits
+            short quantidadeProdutos = 15000;
+            Console.WriteLine(quantidadeProdutos);
+
+            //não é muito usual, mas para não ser confundido com double,
+            //precisa colocar o f
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+
+            Console.ReadLine();
+
+        }
+    }
+}
