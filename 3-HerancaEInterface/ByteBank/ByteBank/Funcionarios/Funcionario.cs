573f598c2aa5c90376a481f8f85034569250604e
MAEDA Priscila (Consulting For ACCOR Hotels) <Priscila.MAEDA@consulting-for.accor.com>
2020-08-28 21:31:31 -0300

    herancas e interface aula 1

---
+++ 3-HerancaEInterface/ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        //0 - funcionario
+        //1 - diretor
+        //2 - designer
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+
+        public double GetBonificacao()
+        {
+            return Salario * 0.10;
+        }
+    }
+}
---
+++ 3-HerancaEInterface/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,34 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario carlos = new Funcionario();
+            carlos.Nome = "Carlos";
+            carlos.CPF = "546.879.157-20";
+            carlos.Salario = 2000;
+
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.CPF = "454.658.148-3";
+            roberta.Salario = 5000;
+
+            gerenciador.Registrar(carlos);
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine("Total de bonificações: " + gerenciador.GetTotalBonificacao());
+            Console.ReadLine();
+        }
+    }
+}
