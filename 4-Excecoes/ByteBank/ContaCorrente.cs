ea4a9d37477d96445bd7041d557174e11404423a
MAEDA Priscila (Consulting For ACCOR Hotels) <Priscila.MAEDA@consulting-for.accor.com>
2020-09-03 21:44:56 -0300

    aulas 1, 2, 3 e 4

---
+++ 4-Excecoes/ByteBank/ContaCorrente.cs
@@ -0,0 +1,87 @@
+﻿// using _05_ByteBank;
+
+using System;
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public static double TaxaOperacao { get; private set; }
+        public static int TotalDeContasCriadas { get; private set; }
+        public Cliente Titular { get; set; }
+
+        public int Numero { get; }
+
+        public int Agencia { get; }
+       
+        private double _saldo = 100;
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            if(agencia <= 0 )
+            {
+                ArgumentException excecao = new ArgumentException("O argumento Agência deve ser maior que zero.", nameof(agencia));
+                throw excecao;
+            }
+
+            if (numero <= 0)
+            {
+                ArgumentException excecao = new ArgumentException("O argumento Número deve ser maior que zero.", nameof(numero));
+                throw excecao;
+            }
+
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalDeContasCriadas++;
+            TaxaOperacao = 30 / TotalDeContasCriadas;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor inválido para o saque", nameof(valor));
+            }
+
+            if (_saldo < valor)
+            {
+                throw new SaldoInsuficienteException(Saldo, valor);
+            }
+
+            _saldo -= valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public void Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor inválido para a transferência", nameof(valor));
+            }
+
+            Sacar(valor);
+            contaDestino.Depositar(valor);
+        }
+    }
+}
---
+++ 4-Excecoes/ByteBank/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ContaCorrente conta = new ContaCorrente(514, 456789);
+                ContaCorrente conta2 = new ContaCorrente(543, 432167);
+
+                conta2.Transferir(-10, conta);
+
+                conta.Depositar(50);
+                Console.WriteLine(conta.Saldo);
+                conta.Sacar(-500);
+
+            }
+            catch (ArgumentException e)
+            {
+                if(e.ParamName == "numero")
+                {
+
+                }
+
+                Console.WriteLine("Argumento com problema: " + e.ParamName);
+                Console.WriteLine("Ocorreu uma exceção do tipo ArgumentException");
+                Console.WriteLine(e.Message);
+            }
+            catch (SaldoInsuficienteException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Exceção do tipo SaldoInsuficiente");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            Console.WriteLine(ContaCorrente.TaxaOperacao);
+
+            Console.ReadLine();
+        }
+    }
+}
