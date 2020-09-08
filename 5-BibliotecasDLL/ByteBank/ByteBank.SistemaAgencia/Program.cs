6cb50c61542abc463b3f05460fed7fa534ac214b
MAEDA Priscila (Consulting For ACCOR Hotels) <Priscila.MAEDA@consulting-for.accor.com>
2020-09-07 22:11:46 -0300

    aula 1

---
+++ 5-BibliotecasDLL/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,24 @@
+﻿using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(123, 456789);
+
+            Funcionario funcionario = null;
+
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
