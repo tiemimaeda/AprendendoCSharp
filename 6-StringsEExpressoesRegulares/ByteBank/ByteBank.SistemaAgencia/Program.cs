d28b362007b69ff465a520afe31bd7450bfaa901
MAEDA Priscila (Consulting For ACCOR Hotels) <Priscila.MAEDA@consulting-for.accor.com>
2020-09-14 22:07:34 -0300

    aulas 1 a 4; Strings e Regex

---
+++ 6-StringsEExpressoesRegulares/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,68 @@
+﻿using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+            //string padrao = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+            //string padrao = "[0-9]{4,5}[-][0-9]{4}";
+            //string padrao = "[0-9]{4,5}[-]{0,1}[0-9]{4}";
+            //string padrao = "[0-9]{4,5}-{0,1}[0-9]{4}";
+            string padrao = "[0-9]{4,5}-?[0-9]{4}";
+
+            string textoDeTeste = "Meu nome é Julia, me ligue em 99898-2241";
+            Match resultado = Regex.Match(textoDeTeste, padrao);
+            
+            Console.WriteLine(resultado.Value);
+            Console.ReadLine();
+
+
+            //--------------------------
+            string urlTeste = "https://www.bytebank.com/cambio";
+            int indiceByteBank = urlTeste.IndexOf("https://www.bytebank.com");
+
+            Console.WriteLine(urlTeste.StartsWith("https://www.bytebank.com"));
+            Console.WriteLine(urlTeste.EndsWith("cambio"));
+            Console.WriteLine(urlTeste.Contains("bytebank"));
+            Console.ReadLine();
+
+
+            //--------------------------
+            string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
+            ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(urlParametros);
+            
+            string valor1 = extratorDeValores.GetValor("moedaDestino");
+            Console.WriteLine("Valor de moedaDestino: " + valor1);
+
+            string valor2 = extratorDeValores.GetValor("moedaOrigem");
+            Console.WriteLine("Valor de moedaOrigem: " + valor2);
+
+            Console.WriteLine(extratorDeValores.GetValor("VALOR"));
+            Console.ReadLine();
+
+
+            //---------------------------
+            ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL("");
+            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
+            int indiceInterrogacao = url.IndexOf('?');
+            Console.WriteLine(indiceInterrogacao);
+
+            Console.WriteLine(url);
+            string argumentos = url.Substring(indiceInterrogacao+1);
+            Console.WriteLine(argumentos);
+
+            Console.ReadLine();
+        }
+    }
+}
