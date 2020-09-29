9868378dd0d523194886e76de93924e082c58f52
Priscila MAEDA <Priscila.MAEDA@consulting-for.accor.com>
2020-09-28 21:30:48 -0300

    List e métodos de extensão

---
+++ 8-ListLambdaELinq/ByteBank/ByteBank.Modelos/OperacaoFincanceiraException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class OperacaoFincanceiraException : Exception
+    {
+        public OperacaoFincanceiraException()
+        {
+
+        }
+
+        public OperacaoFincanceiraException(string mensagem) : base(mensagem)
+        {
+
+        }
+
+        public OperacaoFincanceiraException(string mensagem, Exception excecaoInterna) : base(mensagem, excecaoInterna)
+        {
+
+        }
+    }
+}
